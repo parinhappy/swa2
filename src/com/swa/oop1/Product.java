2c237d07c9beb392048342e07370a9cd814f370c
Parin0901271421 <624259020@webmail.npru.ac.th>
2021-07-13 17:19:26 +0700

    Initial commit

---
+++ src/com/swa/oop1/Product.java
@@ -0,0 +1,13 @@
+package com.swa.oop1;
+
+public class Product {
+    private String productID;
+    private String productName;
+    Product(String pid, String pname){
+      productID = pid;
+      productName = pname;
+    }
+    public String getDetail(){
+      return "Product ID : "+productID+" and Product Name : "+productName;
+    }
+}
---
+++ src/com/swa/oop1/TestDriveClass.java
@@ -0,0 +1,13 @@
+package com.swa.oop1;
+
+public class TestDriveClass {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+            Product p = new Product("P0001","Nintendo Switch");
+            System.out.println(p.getDetail());
+
+
+    }
+
+}
